a848fa789d7f21d9f1f0a62ee0d4b5dcf7373148
Artyom <artychen13@unist.ac.kr>
2019-11-28 11:32:47 +0900

    init structure

---
+++ webpack.config.js
@@ -0,0 +1,92 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+    entry: "./src/index.js",
+    output: {
+        path: path.join(__dirname, "/dist"),
+        filename: "bundle.js"
+    },
+    devServer: {
+      historyApiFallback: true
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: "babel-loader"
+                }
+            },
+            {
+                test:/(\.css|\.scss|\.sass)$/,
+                use: [
+                    {
+                        loader: "style-loader"
+                    }, 
+                    {
+                        loader: "css-loader"
+                    }, 
+                    {
+                        loader: "sass-loader"
+                    }, 
+                ]
+            },
+            {
+                test: /\.(jpe?g|png|gif|ico|mp4|vtt|srt)$/i,
+                use: [
+                  {
+                    loader: 'file-loader',
+                    options: {
+                      name: '[name].[ext]',
+                      publicPath: '/'
+                    }
+                  }
+                ]
+            },
+            {
+                test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+                use: [
+                  {
+                    loader: 'url-loader',
+                    options: {
+                      limit: 10000,
+                      mimetype: 'application/font-woff'
+                    }
+                  }
+                ]
+              },
+              {
+                test: /\.[ot]tf(\?v=\d+.\d+.\d+)?$/,
+                use: [
+                  {
+                    loader: 'url-loader',
+                    options: {
+                      limit: 10000,
+                      mimetype: 'application/octet-stream',
+                      publicPath: '/',
+                    }
+                  }
+                ]
+              },
+              {
+                test: /\.svg(\?v=\d+\.\d+\.\d+)?$/,
+                use: [
+                  {
+                    loader: 'url-loader',
+                    options: {
+                      limit: 10000,
+                      mimetype: 'image/svg+xml'
+                    }
+                  }
+                ]
+              },
+        ] 
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+          template: "./src/index.html"
+      })  
+    ]
+};
