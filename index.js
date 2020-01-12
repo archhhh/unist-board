d28ffffc25cc8bb59d542c31d3c3a3966ba3d2f7
artyom <artyomniyritsar@algorima.io>
2020-01-12 23:14:54 +0900

    set up http routing, removed unneccessary deps

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // node.js server
+const path = require('path');
+const express = require('express');
+const app = express()
+const port = 3000;
+
+app.use(express.static('dist'));
+
+app.get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
+});
+
+app.listen(port, () => console.log('...listening'));
